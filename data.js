3b2081cde6c7bc9eb0e40e0329cdace7e5dbc811
TabishB <tabishbidiwale@gmail.com>
2019-01-31 22:57:17 +1100

    basic word cloud

---
+++ data.js
@@ -0,0 +1,16 @@
+var data = [
+    { "key": "Stocktaking", "value": "20" },
+    { "key": "Receipting", "value": "13" },
+    { "key": "Issuing", "value": "15" },
+    { "key": "Discrepancy Management", "value": "14" },
+    { "key": "SCA Management", "value": "15" },
+    { "key": "Disposals", "value": "16" },
+    { "key": "Purchasing", "value": "17" },
+    { "key": "LSA", "value": "15" },
+    { "key": "Loans", "value": "14" },
+    { "key": "Codification", "value": "14" },
+    { "key": "Inventory Management", "value": "15" },
+    { "key": "IMCAD", "value": "14" },
+    { "key": "Improved Stock Management (ISM)", "value": "16" },
+    { "key": "WMS JLC ONLY", "value": "14" }
+];
